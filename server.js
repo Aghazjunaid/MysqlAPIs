2817ebd85049b891e91c7fd60418adb6f7227b0f
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-12-22 18:47:20 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log("server up and running on PORT :", port);
+});
